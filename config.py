14f4147d381b147e03b62f699ed6eda73a12d9e1
PRASHANT SHARMA <prashant.dec25@gmail.com>
2021-03-15 18:05:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,16 @@
+'''
 customhost = "employee.coghw13fheqo.us-east-2.rds.amazonaws.com"
 customuser = "intellipaat"
 custompass = "intellipaat123"
 customdb = "employee"
 custombucket = "addemployee"
 customregion = "us-east-2"
+'''
+import os
+
+customhost = os.environ["DB_HOST"]
+customuser = os.environ["DB_USER"]
+custompass = os.environ["DB_PASS"]
+customdb =  os.environ["DB_NAME"]
+custombucket = os.environ["AWS_S3_BUCKET"]
+customregion = os.environ["AWS_REGION"]
